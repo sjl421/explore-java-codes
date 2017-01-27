56ae6fdaf32f79f93c34a66dc278a8f57e6b2edb
Ethan <shuyouxin1@gmail.com>
2017-01-27 12:20:21 +0800

    add HelloWorld

---
+++ src/main/java/com/explorejava/codes/introduction/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.explorejava.codes.introduction;
+
+
+/**
+ * @author Ethan
+ * @version V1.0
+ */
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
